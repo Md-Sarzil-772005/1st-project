2c821309c4559656862453a40fd84ff530f5eaf6
Md Sarzil Anam  Chowdhury <mdsarzilanamchowdhury@gmail.com>
2025-07-22 21:23:28 +0600

    Leapyear
    Leap Year

--- Leapyear.java
+++ Leapyear.java
@@ -5,7 +5,7 @@ public class LeapYear
     public static void main(String[] args) 
       {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a year");
+        System.out.print("Enter any year");
         int year = input.nextInt();
         
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
