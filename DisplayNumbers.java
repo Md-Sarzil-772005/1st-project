e1483edf70b38bf6e645ccc7956a1be630abd6c2
Md Sarzil Anam  Chowdhury <mdsarzilanamchowdhury@gmail.com>
2025-07-23 00:46:09 +0600

    Display Numbers
    Display of numbers

--- DisplayNumbers.java
+++ DisplayNumbers.java
@@ -1,10 +1,10 @@
 public class DisplayNumbers 
 {
-    public static void main(String[] args) 
+  public static void main(String []args) 
     {
         for (int i = 1; i <= 100; i++) 
         {
-            System.out.println(i);
+           System.out.println(i);
         }
     }
 }
