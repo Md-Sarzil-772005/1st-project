f27329f46f400656111ba6825425152ca1e35550
Md Sarzil Anam  Chowdhury <mdsarzilanamchowdhury@gmail.com>
2025-07-22 20:43:45 +0600

    Helllo World
    Helllo World

--- Index.java
+++ Index.java
@@ -2,7 +2,7 @@ public class Index
 {
     public static void main(String[]args)
   {
-       system.out.println("Hello world!");
+       system.out.println("Hello World");
        system.out.println("Sarzil Anam Chowdhuury");
   }
 }
