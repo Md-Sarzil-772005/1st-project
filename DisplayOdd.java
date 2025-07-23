2bd5518d31bf21ccb79085afd9d6e3e8864cece6
Md Sarzil Anam  Chowdhury <mdsarzilanamchowdhury@gmail.com>
2025-07-23 11:34:06 +0600

    Displaying Odd
    Display of Odd numbers

--- DisplayOdd.java
+++ DisplayOdd.java
@@ -2,7 +2,7 @@ public class DisplayOdd
 {
     public static void main(String[] args) 
     {
-        for (int i = 1; i <= 100; i=i+2) 
+      for (int i = 1; i <= 100; i=i+2) 
         {
             System.out.println(i);
         }
